a749e8abd331847ae4d8b65a0c51677e7ada49d7
Денис Джабаров <dgd@crtweb.ru>
2022-03-13 18:14:12 +0500

    Changed function.php

--- function.php
+++ function.php
@@ -1,7 +1,11 @@
 <?php
 
+// Эта функция используется для дебага
 function debug ($v) {
     echo '<pre>';
+    if (!is_array($v))
+        $v = [$v];
     print_r($v);
     echo '</pre>';
+    // Я крутой челн
 }
